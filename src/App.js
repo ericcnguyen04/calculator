63b8e237780c8be72cb1c904a942aeaecfcc39fd
ericcnguyen04 <ericc.win@gmail.com>
2023-07-17 22:28:20 -0400

    wait

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
         Calculator
+        </h1>
+        <div>
+carj tni
+        </div>
       </header>
     </div>
   );
