90954e5657be5a10d3bf98767899163355fd656a
ericcnguyen04 <ericc.win@gmail.com>
2023-07-19 07:16:35 -0400

    reformat

--- src/App.js
+++ src/App.js
@@ -14,22 +14,21 @@ function App() {
             69
           </div>
           <div className='off-screen'>
-
-            <div className='numbers'>
-              <div className='1'>1</div>
-              <div className='2'>2</div>
-              <div className='3'>3</div>
-              <div className='4'>4</div>
-              <div className='5'>5</div>
-              <div className='6'>6</div>
-              <div className='7'>7</div>
-              <div className='8'>8</div>
-              <div className='9'>9</div>
-              <div className='0'>0</div>
-            </div>
-            <div className='operators'>
-              +-x/
-            </div>
+            <div className='1'>1</div>
+            <div className='2'>2</div>
+            <div className='3'>3</div>
+            <div className='4'>4</div>
+            <div className='5'>5</div>
+            <div className='6'>6</div>
+            <div className='7'>7</div>
+            <div className='8'>8</div>
+            <div className='9'>9</div>
+            <div className='0'>0</div>
+            <div className='+'>+</div>
+            <div className='-'>-</div>
+            <div className='*'>*</div>
+            <div className='/'>/</div>
+            <div className='='>=</div>
           </div>
         </div>
       </header>
