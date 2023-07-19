1f4781eae0bb4869aac4db18c872e8416a1f3a09
ericcnguyen04 <ericc.win@gmail.com>
2023-07-19 07:13:38 -0400

    numbers

--- src/App.js
+++ src/App.js
@@ -16,7 +16,16 @@ function App() {
           <div className='off-screen'>
 
             <div className='numbers'>
-              123789
+              <div className='1'>1</div>
+              <div className='2'>2</div>
+              <div className='3'>3</div>
+              <div className='4'>4</div>
+              <div className='5'>5</div>
+              <div className='6'>6</div>
+              <div className='7'>7</div>
+              <div className='8'>8</div>
+              <div className='9'>9</div>
+              <div className='0'>0</div>
             </div>
             <div className='operators'>
               +-x/
