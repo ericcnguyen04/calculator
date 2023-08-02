8d77138bd8cab67c5812ed9d015395c7c2a9aad2
ericcnguyen04 <ericc.win@gmail.com>
2023-08-02 15:16:37 -0400

    applied to other opperators

--- src/App.js
+++ src/App.js
@@ -72,18 +72,18 @@ function App() {
             <div className='q2 button' onClick={() => handleNumberClick(2)}>2</div>
             <div className='q3 button' onClick={() => handleNumberClick(3)}>3</div>
             
-            <div className='q* button'>*</div>
+            <div className='q* button' onClick={() => handleOperator('*')}>*</div>
             
             <div className='q4 button' onClick={() => handleNumberClick(4)}>4</div>
             <div className='q5 button' onClick={() => handleNumberClick(5)}>5</div>
             <div className='q6 button' onClick={() => handleNumberClick(6)}>6</div>
             
-            <div className='q- button'>-</div>
+            <div className='q- button' onClick={() => handleOperator('-')}>-</div>
             
             <div className='q7 button' onClick={() => handleNumberClick(7)}>7</div>
             <div className='q8 button' onClick={() => handleNumberClick(8)}>8</div>
             <div className='q9 button' onClick={() => handleNumberClick(9)}>9</div>
-            <div className='q+ button'>+</div>
+            <div className='q+ button' onClick={() => handleOperator('+')}>+</div>
 
             <div className='q0 button' onClick={() => handleNumberClick(0)}>0</div>
             <div className='q. button' onClick={() => handleNumberClick('.')}>.</div>
